c30c5b292bf211040b1c8b4a0758aba7d69b2aef
Piotr Krzyminski <piotr.krzyminski@hycom.pl>
2018-08-22 14:40:05 +0200

    Initial commit

---
+++ milionerzy-web/src/main/java/pl/milionerzy/WebApplication.java
@@ -0,0 +1,15 @@
+package pl.milionerzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Piotr Krzyminski
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
